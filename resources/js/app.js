28d57eeee464e813bb64675deac8b8d6d040608c
evgeny <kas0733@gmail.com>
2022-11-05 21:11:15 +0500

    feature: Подключить и настроить реакт

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,45 @@
-require('./bootstrap');
+import React from "react";
+import ReactDOM from 'react-dom';
+import axios from "axios";
+import NotificationContainer from "react-notifications/lib/NotificationContainer";
+import {createNotification, router} from "./helpers"
+import {RouterProvider} from "react-router-dom";
+
+axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = token;
+    }
+
+    if (!config.headers.ContentType) {
+        config.headers.ContentType = `application/json; charset=UTF-8`;
+    }
+
+    config.headers.Accept = `application/json`;
+
+    return config;
+}, function (error) {
+    createNotification("error", error.message);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+
+    return response;
+}, function (error) {
+    createNotification("error", error.message);
+    return Promise.reject(error);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    createNotification("info", "loaded");
+})
+
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+        <NotificationContainer/>
+    </React.StrictMode>,
+    document.querySelector("#App")
+);
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .react()
+    .extract(['react'])
+    .version();
+
+mix.sass('resources/sass/app.sass', 'public/css')
+    .version();
